e5e564fd21f03d0e40f9793c7bde9ded78263a3d
Carine Litwinczuk <carine4630@gmail.com>
2026-02-14 15:29:02 +0100

    Ajustando Nomes de botoes, titulos, legandas de eixos e erros de portugues.

--- app.py
+++ app.py
@@ -3,21 +3,43 @@ import streamlit as st
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles.csv') #lendo os dados do arquivo.
-st.header('Anúncios de vendas de carro')
 
-build_histogram = st.checkbox('Criar uma histograma que comparar Condição do Veículo Vs Preço.')
 
-if build_histogram: #se a caixa for selecionada.
-    st.write('Criando histograma para dados de anúncios de vendas de carro') #o que acontece se clincar no botão.
-    car_data_tips = px.data.tips()
-    fig = px.histogram(car_data , x = "condition", y = "price", color = 'condition', color_discrete_sequence=px.colors.qualitative.Set1) #criando histograma com as condições que os veículos estão VS o preço.
-    st.plotly_chart(fig , use_container_width=True)#exibindo o grafico em streamlit
+build_histogram = st.checkbox('Click para criar uma histograma que compare as Condição do Veículo Vs Preço.')
+if build_histogram: #se essa caixa for selecionada.
+st.header('Comparações de carros anúnciados para venda') #titulo do dashboard.
+st.write('Criando histograma comparando as Condições do Veículo anúnciados VS o preço.') #o que acontece se clincar no botão.
+fig = px.histogram(
+       car_data,
+       x = "condition",
+       y = "price",
+       title='Condição do Veículo VS Preço',
+       color = 'condition',
+       color_discrete_sequence=px.colors.qualitative.Set1
+       ) #criando histograma com as condições que os veículos estão VS o preço.
+fig.update_layout(
+    xaxis_title="Condição do Veículo",
+    yaxis_title="Preço"
+    )#Eixos, etc.
+st.plotly_chart(fig , use_container_width=True)#exibindo o grafico em streamlit
 
-build_scatter = st.checkbox('Criar um gráfico de dispersão entre Modelo de Carro VS Odômetro')
+build_scatter = st.checkbox('Criando um gráfico de dispersão entre Modelo de Carro VS Odômetro')
 
 if build_scatter: # se a caixa for selecionada
-  st.write('Criando um gráfico de dispersão para ver se a qua')
-  fig = px.scatter(car_data, x= "model", y="odometer", color='model',color_discrete_sequence=px.colors.qualitative.Set1)
-  fig.update_layout(xaxis_tickangle=45,title= "Relação entre Modelo e Odômetro", xaxis_title="Modelo de Veículo", yaxis_title="Odômetro")#Criando umma aparencia melhor para analisar entre 
-  st.plotly_chart(fig, use_container_width=True) #exibindo
+st.write('Criando um gráfico de dispersão para ver se a qua')
+fig = px.scatter(
+    car_data,
+    x= "model",
+    y="odometer",
+    color='model',
+    color_discrete_sequence=px.colors.qualitative.Set1
+    )#Criando um gráfico de dispersão para ver se a qualidade do modelo tem relação com o odômetro.
+fig.update_layout(
+    xaxis_tickangle=45,
+    title= "Relação entre Modelo e Odômetro",
+    xaxis_title="Modelo de Veículo",
+    yaxis_title="Odômetro"
+    )#Titulos,Eixos, etc.
+fig.update_layout(width=1400, height=1000) #Aumentando o tamanho do grafico.
+st.plotly_chart(fig, use_container_width=True) #exibindo
  
