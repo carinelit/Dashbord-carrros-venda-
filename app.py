8d4d47db90c939328d71588c7cf6c76f5105f0a4
Carine Litwinczuk <carine4630@gmail.com>
2026-02-14 13:06:06 +0100

    Arrumando o gráfico de dispersão, agora ele compara odômetro com modelo de veiculo.

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import plotly.express as px
 car_data = pd.read_csv('vehicles.csv') #lendo os dados do arquivo.
 st.header('Anúncios de vendas de carro')
 
-build_histogram = st.checkbox('Criar um histograma')
+build_histogram = st.checkbox('Criar uma histograma que comparar Condição do Veículo Vs Preço.')
 
 if build_histogram: #se a caixa for selecionada.
-    st.write('Criando histograma para dados de anúncios de vendas de carro') #o que vai acontecer se clincar no botão.
-    fig= px.histogram(car_data, x="model_year") #montando o histograma
+    st.write('Criando histograma para dados de anúncios de vendas de carro') #o que acontece se clincar no botão.
+    car_data_tips = px.data.tips()
+    fig = px.histogram(car_data , x = "condition", y = "price", color = 'condition', color_discrete_sequence=px.colors.qualitative.Set1) #criando histograma com as condições que os veículos estão VS o preço.
     st.plotly_chart(fig , use_container_width=True)#exibindo o grafico em streamlit
 
-build_scatter = st.checkbox('Criar um gráfico de dispersão')
+build_scatter = st.checkbox('Criar um gráfico de dispersão entre Modelo de Carro VS Odômetro')
 
 if build_scatter: # se a caixa for selecionada
-  st.write('Criando um gráfico de dispersão para todos os anúncios de vendas de carro')
-  fig = px.scatter(car_data, x= "odometer", y="price") #criando gráfico de dispersão com valor do odômetro vs preço.
+  st.write('Criando um gráfico de dispersão para ver se a qua')
+  fig = px.scatter(car_data, x= "model", y="odometer", color='model',color_discrete_sequence=px.colors.qualitative.Set1)
+  fig.update_layout(xaxis_tickangle=45,title= "Relação entre Modelo e Odômetro", xaxis_title="Modelo de Veículo", yaxis_title="Odômetro")#Criando umma aparencia melhor para analisar entre 
   st.plotly_chart(fig, use_container_width=True) #exibindo
  
